39aeea30fa410d6de4227a67974f047234b524cc
Megh <megh@logicrays.com>
2025-03-12 14:24:10 +0530

    fix

--- src/database/prisma/prisma.service.ts
+++ src/database/prisma/prisma.service.ts
@@ -13,14 +13,12 @@ export class PrismaService
   implements OnModuleInit, OnModuleDestroy
 {
   async onModuleInit() {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
     await this.$connect();
-    Logger.log('PrismaService connected', 'PrismaService');
+    Logger.log('Database connected successfully!', 'PrismaService');
   }
 
   async onModuleDestroy() {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
     await this.$disconnect();
-    Logger.log('PrismaService disconnected', 'PrismaService');
+    Logger.log('Database disconnected successfully!', 'PrismaService');
   }
 }
